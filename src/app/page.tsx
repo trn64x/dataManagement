91631543a50ae2abf7d69b58343af5cbd0492d88
Robert Plawski <dev@robertplawski.pl>
2025-04-01 19:33:03 +0200

    Fixed type errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
-import GridBackground from "@/components/background/GridBackground";
-import CtaButton from "@/components/buttons/CtaButton";
 import Header from "@/components/header/Header";
 import BrandWave from "@/components/pearls/BrandWave";
 import HeroSection from "@/components/sections/HeroSection";
-import TextGradient from "@/components/text/TextGradient";
-import { Fade } from "react-awesome-reveal";
-
 
 export default function Home() {
   return (
     <>
       <Header />
-      <HeroSection/>
-      <BrandWave/>
+      <HeroSection />
+      <BrandWave />
     </>
   );
 }
