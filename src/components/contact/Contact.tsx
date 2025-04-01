81ef8f583965d7745bd761b2fe637b5ee8dd3659
Robert Plawski <dev@robertplawski.pl>
2025-04-01 20:06:47 +0200

    Fixed mobile + changed schema + changed formatting

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -18,37 +18,39 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
-import {contactFormSchema} from "@/schemas/contactFormSchema";
+import { contactFormSchema } from "@/schemas/contactFormSchema";
 import { useCallback, useMemo, useState } from "react";
-import { FileWarning, Loader2, MailWarningIcon } from "lucide-react";
-
+import { Loader2, MailIcon, MailWarningIcon, Send } from "lucide-react";
+import { Label } from "../ui/label";
 
 const useContact = () => {
-  const [message,setMessage] = useState("Skontaktuj się!");
-  const [loading,setLoading] = useState(false);
+  const [message, setMessage] = useState("Skontaktuj się!");
+  const [loading, setLoading] = useState(false);
   const [statusCode, setStatusCode] = useState(200);
 
-  const isOK = useMemo(()=>statusCode==200,[statusCode])
+  const isOK = useMemo(() => statusCode == 200, [statusCode]);
 
-  const onSubmit = useCallback(async (data: any) => {
-    setLoading(true)
-    const res = await fetch('/api/contact', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-    const json = await res.json();
-    setStatusCode(res.status)
-    setMessage(json.message)
-    setLoading(false)
+  const onSubmit = useCallback(
+    async (data: unknown) => {
+      setLoading(true);
+      const res = await fetch("/api/contact", {
+        method: "POST",
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const json = await res.json();
+      setStatusCode(res.status);
+      setMessage(json.message);
+      setLoading(false);
+    },
+    [setMessage, setLoading, setStatusCode]
+  );
 
-  },[setMessage,setLoading,setStatusCode]);
-
-  return {onSubmit,loading,message,statusCode, isOK}
-}
+  return { onSubmit, loading, message, statusCode, isOK };
+};
 
 export default function Contact() {
   const form = useForm({
@@ -60,13 +62,12 @@ export default function Contact() {
     },
   });
 
-  const {message, loading,statusCode, isOK, onSubmit} = useContact()
+  const { message, loading, isOK, onSubmit } = useContact();
 
   return (
     <>
-      
       <main className="font-sans gap-8 p-8 flex flex-col gap-4 min-h-[80vh] items-center justify-center">
-        <div className="flex flex-col w-full gap-16 xl:flex-row justify-center items-center">
+        <div className="flex flex-col w-full gap-16  justify-center items-center">
           <div className="flex flex-col gap-2 sm:gap-4">
             {/* Black Text */}
             <Fade
@@ -93,7 +94,10 @@ export default function Contact() {
           </div>
           <div className="flex flex-col gap-4 w-64 sm:w-86 sm:w-96 md:w-120">
             <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
                 {/* Username Field */}
                 <FormField
                   control={form.control}
@@ -102,7 +106,11 @@ export default function Contact() {
                     <FormItem>
                       <FormLabel>Imię</FormLabel>
                       <FormControl>
-                        <Input className="input-focus" placeholder="Wpisz swoje imię" {...field} />
+                        <Input
+                          className="input-focus"
+                          placeholder="Wpisz swoje imię"
+                          {...field}
+                        />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
@@ -116,7 +124,12 @@ export default function Contact() {
                     <FormItem>
                       <FormLabel>Email</FormLabel>
                       <FormControl>
-                        <Input type="email" className="input-focus" placeholder="example@example.com" {...field} />
+                        <Input
+                          type="email"
+                          className="input-focus"
+                          placeholder="jan.kowalski@eksabajt.pl"
+                          {...field}
+                        />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
@@ -130,18 +143,33 @@ export default function Contact() {
                     <FormItem>
                       <FormLabel>Wiadomość</FormLabel>
                       <FormControl>
-                        <Textarea className="input-focus" placeholder="Miejsce na twoją wiadomość..." {...field} />
+                        <Textarea
+                          className="input-focus"
+                          placeholder="Miejsce na twoją wiadomość..."
+                          {...field}
+                        />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
                   )}
                 />
-                <div className="flex flex-row ">
-                  <div className="flex items-center justify-start w-8/10">
-                    {loading ? <Loader2 className="animate-spin"/> : <p className={!isOK ? "flex-row flex gap-2 font-bold" : ""}>{!isOK && <MailWarningIcon/>} {message}</p>}
+                <div className="flex flex-col sm:flex-row gap-4 ">
+                  <div className="flex  items-center justify-start flex-8/10">
+                    {loading ? (
+                      <Loader2 className="animate-spin" />
+                    ) : (
+                      <Label
+                        className={
+                          !isOK ? " flex-row  flex gap-2 font-bold" : ""
+                        }
+                      >
+                        {isOK ? <MailIcon /> : <MailWarningIcon />} {message}
+                      </Label>
+                    )}
                   </div>
-                  <div className="flex items-center justify-end w-2/10">
-                    <Button className="w-32" type="submit">
+                  <div className="flex items-center justify-end flex-2/10">
+                    <Button className="flex-1 cursor-pointer" type="submit">
+                      <Send />
                       Wyślij
                     </Button>
                   </div>
@@ -151,7 +179,6 @@ export default function Contact() {
           </div>
         </div>
       </main>
-      
     </>
   );
 }
--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,76 +1,80 @@
-"use server"
+"use server";
 
-// DO IT BETER
+// DO IT BETTER
 
-import {z} from "zod"
-import rateLimit from 'express-rate-limit'
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { contactFormSchema } from "@/schemas/contactFormSchema"
+import rateLimit from "express-rate-limit";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { contactFormSchema } from "@/schemas/contactFormSchema";
 
 type ResponseData = {
-  message: string,
+  message: string;
   errors?: {
-    username?: string[],
-    email?: string[],
-    message?: string[]
-  }
-}
+    username?: string[];
+    email?: string[];
+    message?: string[];
+  };
+};
 
 const apiLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minut
-    max: 5, // 5 wiadomości 
-    message: '{"message": "Spróbuj ponownie później..."}',
-  });
+  windowMs: 15 * 60 * 1000, // 15 minut
+  max: 5, // 5 wiadomości
+  message: '{"message": "Spróbuj ponownie później..."}',
+});
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  
-    if (req.method !== 'POST') {
-        res.status(405).send({ message: 'Only POST requests allowed' })
-        return
+  if (req.method !== "POST") {
+    res.status(405).send({ message: "Only POST requests allowed" });
+    return;
+  }
+
+  apiLimiter(req, res, async () => {
+    const contactFormData = req.body;
+    const validatedContactFormData =
+      contactFormSchema.safeParse(contactFormData);
+    const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+
+    if (!validatedContactFormData.success) {
+      const formFieldErrors =
+        validatedContactFormData.error.flatten().fieldErrors;
+
+      res.status(400).send({
+        message: "Form invalid",
+        errors: {
+          username: formFieldErrors?.username,
+          email: formFieldErrors?.email,
+          message: formFieldErrors?.message,
+        },
+      });
+
+      return;
     }
 
-    apiLimiter(req, res, async () => {
-        const contactFormData = req.body;
-        const validatedContactFormData = contactFormSchema.safeParse(contactFormData);
-        const ip =  req.headers['x-forwarded-for'] || req.socket.remoteAddress 
-
-        if (!validatedContactFormData.success) {
-          const formFieldErrors =
-            validatedContactFormData.error.flatten().fieldErrors;
-    
-          res.status(400).send({message:"Form invalid", errors: {
-            username: formFieldErrors?.username,
-            email: formFieldErrors?.email,
-            message: formFieldErrors?.message,
-          }})
-    
-          return
-        }
-
-        const {ok} = await fetch(process.env.CONTACT_WEBHOOK_URL as string,{
-            method: 'POST',
-            headers: {
-              'Accept': 'application/json, text/plain, */*',
-              'Content-Type': 'application/json'
+    const { ok } = await fetch(process.env.CONTACT_WEBHOOK_URL as string, {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        embeds: [
+          {
+            author: {
+              name: `${contactFormData.username} <${contactFormData.email}> [${ip}]`,
             },
-            body: JSON.stringify({
-              embeds:[{
-                author:{name:`${contactFormData.username} <${contactFormData.email}> [${ip}]`},
-                title:contactFormData.message
-              }],
-              timestamp: new Date().toISOString()
-          })
-          }
-        )
-        if(!ok){
-          res.status(500).json({message:"Błąd podczas wysyłania wiadomości"});
-          return
-        }
-
-        res.status(200).json({ message: "Pomyślnie wysłano..."})
+            title: contactFormData.message,
+          },
+        ],
+        timestamp: new Date().toISOString(),
+      }),
     });
+    if (!ok) {
+      res.status(500).json({ message: "Błąd podczas wysyłania wiadomości" });
+      return;
+    }
 
- }
+    res.status(200).json({ message: "Pomyślnie wysłano..." });
+  });
+}
