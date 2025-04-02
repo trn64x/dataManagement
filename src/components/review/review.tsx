b6a428b8d289572e13148cabdd2c8a5914f2d1ff
unknown <maciekp371@gmail.com>
2025-04-02 12:02:45 +0200

    bardzo swiadomy design z nutką akcentu zmiany koloru na tekscie (z wloskiego gradient)

--- src/components/review/review.tsx
+++ src/components/review/review.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState} from 'react'
 import jsonFile from './data.json'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
+import Fade from "../pearls/Fade";
+import TextGradient from "../text/TextGradient";
 type jsonFileType = {
     name: string,
     content: string,
@@ -24,18 +26,30 @@ useEffect(()=>{
 ,[responding])
 
 const arrays = (stars:number) => {
-    return Array(stars).fill(<FontAwesomeIcon className="text-xl max-lg:text-xs max-md:text-2xl text-green-500" icon={faStar} />)
+    return Array(stars).fill(<FontAwesomeIcon className="text-xl max-lg:text-lg max-md:text-2xl text-green-500" icon={faStar} />)
     .map((star,index) => <span key={index}>{star}</span>)
 }
 
     return( 
 <>
 <div className="min-h-[400px]">
-    <div className="h-[100px] flex justify-center items-center text-5xl font-black">Our Reviews</div>
+    
+    <div className="h-[100px] flex justify-center items-center text-5xl max-lg:text-4xl max-md:text-2xl mad-sm:text-base font-black"><div className="pb-2 mr-[10px] text-5xl max-lg:text-4xl max-md:text-2xl mad-sm:text-lg">Co myślą o nas</div>  <TextGradient>
+              {" "}
+              <Fade
+                triggerOnce
+                delay={500}
+                className="font-black text-5xl max-lg:text-4xl max-md:text-2xl mad-sm:text-lg"
+                cascade
+                damping={0.05}
+              >
+                nasi klienci
+              </Fade>
+            </TextGradient></div>
     <div className="min-h-[300px] flex flex-row flex-wrap justify-around items-center">
 {jsonList?.map((value,index)=> {
     return(
-        <div className="h-[250px] rounded-2xl p-4 w-[30vw] max-md:w-[100vw] max-md:m-5 max-lg:h-[350px] max-md:h-[300px]  max-lg:my-5" key={index}><div className='flex flex-row justify-between'><div className="w-[70%] text-2xl my-3 text-stone-850 font-bold max-md:text-3xl max-md:m-0">{value.name}</div><div className=' flex justify-center items-center text-xl word-[10px] font-bold text-center max-md:w-[30%]'>{arrays(value.stars)}</div></div><div className="p-1 max-md:my-4 text-xl leading-10 max-xl:leading-7">{value.content}</div></div>
+        <div className="h-[250px] rounded-2xl p-4 w-[30vw] max-md:w-[100vw] max-md:min-h[150px] max-md:m-3  max-lg:p-1 max-lg:my-3" key={index}><div className='flex flex-row justify-between'><div className="w-[100%] text-2xl my-3 text-stone-850 font-bold max-md:text-3xl max-md:m-0">{value.name}</div><div className=' flex justify-center items-center text-xl word-[10px] font-bold text-center max-md:w-[30%]'>{arrays(value.stars)}</div></div><div className="p-1 max-md:m-4 text-xl leading-10 max-xl:leading-7 text-wrap">{value.content}</div></div>
     );
 })}
     </div>
