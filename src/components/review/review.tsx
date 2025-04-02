4af8767d9e73b17f82b288d10fb8e950ff1cd643
unknown <maciekp371@gmail.com>
2025-04-02 13:18:23 +0200

    -

--- src/components/review/review.tsx
+++ src/components/review/review.tsx
@@ -12,7 +12,7 @@ const arrays = (stars:number) => {
 
     return( 
 <>
-<div className="min-h-[400px] w-max">
+<div className="min-h-[400px] max-w">
     
     <div className="h-[100px] flex justify-center items-center text-5xl mad-sm:text-base max-md:text-2xl max-lg:text-4xl font-black"><div className="pb-2 mr-[10px] text-5xl max-sm:text-xl max-md:text-2xl max-lg:text-4xl">Co myślą o nas</div>  <TextGradient>
               {" "}
@@ -26,10 +26,10 @@ const arrays = (stars:number) => {
                 nasi klienci
               </Fade>
             </TextGradient></div>
-    <div className="min-h-[250px] flex flex-row flex-wrap justify-around items-center">
+    <div className="min-h-[250px] max-w flex flex-row flex-wrap justify-center items-start">
 {jsonFile?.map((value,index)=> {
     return(
-        <div className="h-[100%] p-4  rounded-2xl w-[30vw] max-md:w-[100%] max-md:min-h[150px] max-md:m-3  max-lg:p-1" key={index}><div className='flex flex-row justify-between'><div className="w-[100%] text-3xl my-3 text-stone-850 font-black max-md:m-0">{value.name}</div><div className=' flex justify-center items-center text-xl font-bold text-center'>{arrays(value.stars)}</div></div><div className="p-1 max-md:m-2 text-xl leading-10 max-md:leading-7 text-wrap">{value.content}</div></div>
+        <div className="h-[100%] p-4 my-3 rounded-2xl w-[33%] max-md:w-[100%] max-md:min-h[150px]  max-lg:p-1" key={index}><div className='flex flex-row justify-between'><div className="flex justify-start items-center w-[100%] text-3xl my-3 text-stone-850 font-black max-md:text-2xl max-md:m-0">{value.name}</div><div className=' flex justify-center items-center my-3  text-xl font-bold text-center'>{arrays(value.stars)}</div></div><div className="p-1 text-xl leading-10 max-md:leading-7 text-wrap max-md:text-base">{value.content}</div></div>
     );
 })}
     </div>
